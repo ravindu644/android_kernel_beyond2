a10af0153e6d290a5ba4320d207673f5d2e528ae
@ravindu644 <droidcasts@protonmail.com>
2024-03-05 12:08:03 +0530

    Actually, we don't need to build dtbo
    	modified:   build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -30,7 +30,6 @@ rm -rf out && mkdir out
 
 dtb_img() {
 	chmod +777 $dt_tool/* -R
-	$dt_tool/mkdtimg cfg_create $work_dir/out/dtbo_beyond2.img $dt_tool/beyond2.cfg -d $work_dir/arch/arm64/boot/dts/samsung
 	$dt_tool/mkdtimg cfg_create $work_dir/out/dtb_beyond2.img $dt_tool/exynos9820.cfg -d $work_dir/arch/arm64/boot/dts/exynos
 	
 	}
